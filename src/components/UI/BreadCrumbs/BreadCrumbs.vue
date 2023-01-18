3d51ea51d8af0f1408922b339c45e558865c5ab3
Jasurbek Abdukhalikov <jasurbek.abdukhalikov@gmail.com>
2023-01-18 12:24:42 +0500

    BreadCrumbs component

---
+++ src/components/UI/BreadCrumbs/BreadCrumbs.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+defineProps({
+  links: Array
+})
+</script>
+
+<template>
+  <div class="bread-crumbs">
+    <div
+        v-for="(item, index) in links"
+        :key="index"
+        class="bread-crumbs__item"
+    >
+      <router-link
+          :class="[
+                'bread-crumbs__link',
+                {
+                  'bread-crumbs__active-page': item.active
+                }
+            ]"
+          :to="item.href"
+          tag="button"
+      >
+        {{ item.text }}
+      </router-link>
+      <i v-show="!item.active" class="bread-crumbs__arrow">
+        <svg data-src="/img/icons/arrowRight.svg"/>
+      </i>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./BreadCrumbs.scss"/>
